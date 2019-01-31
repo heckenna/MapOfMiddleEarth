03e651c89560dbc7536bb8d82d34e580230944ef
heckenna <heckenna@rose-hulman.edu>
2019-01-31 12:51:06 -0500

    Git rid of scan

--- MiddleEarthGraphProject/src/GetUserInput.java
+++ MiddleEarthGraphProject/src/GetUserInput.java
@@ -1,7 +1,5 @@
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -95,44 +93,4 @@ public class GetUserInput extends JComponent{
 		this.panel.add(time);
 		
 	}
-	
-	public ArrayList<String> scan() {
-		//System.out.println("scanning");
-		
-		
-		
-		ArrayList<String> places = new ArrayList<String>();
-		
-		Scanner in = new Scanner(System.in);
-		System.out.print("What location do you wish to start at?");
-		
-		String s = in.nextLine();
-		while (!middleEarth.hasPlace(s)) {
-			System.out.print("That location does not exist. Please pick a valid location.");
-			s = in.nextLine();
-		}
-		places.add(s);
-		
-		System.out.print("What location do you wish to end at?");
-		
-		String t = in.nextLine();
-		while (!middleEarth.hasPlace(t)) {
-			System.out.print("That location does not exist. Please pick a valid location.");
-			t = in.nextLine();
-		}
-		places.add(t);
-		
-		System.out.print("Which metric would you like to use? time or distance?");
-		
-		String metric = in.nextLine();
-		while (!metric.equals("time") && !metric.equals("distance")) {
-			System.out.print("That metric does not exist. Please pick a valid metric.");
-			metric = in.nextLine();
-		}
-		places.add(metric);
-		return places;
-	}
-	
-	
-	
 }
